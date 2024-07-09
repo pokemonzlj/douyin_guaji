359001f534c6aa798472e4ca63b9e4e9430c7401
叫我大叼哥 <35096840+pokemonzlj@users.noreply.github.com>
2024-07-09 16:30:32 +0800

    Update douyin_guaji.py

--- douyin_guaji.py
+++ douyin_guaji.py
@@ -11,11 +11,7 @@ class fudai_guaji:
         """根据自己设备的分辨率，修改高度的偏移值"""
         device_id = self.analyser.select_device()
         if device_id:
-            if device_id == "XXXXX":
-                y_pianyi = 26
-                self.analyser.fudai_choujiang(device_id, y_pianyi, False, 5)
-            else:
-                self.analyser.fudai_choujiang(device_id, y_pianyi, True, 5)
+            self.analyser.fudai_choujiang(device_id, y_pianyi, False, 5)
             return True
         time.sleep(10)
         self.guaji(y_pianyi)
@@ -23,4 +19,4 @@ class fudai_guaji:
 
 if __name__ == '__main__':
     choujiang = fudai_guaji()
-    choujiang.guaji(0)  # 存在偏移的在这里填值
+    choujiang.guaji(0)  # 存在偏移的在这里填值既可
