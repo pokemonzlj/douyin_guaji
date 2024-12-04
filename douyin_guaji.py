31dcb54eb16607d53b5a0454fe62bac063889af9
叫我_叼哥 <35096840+pokemonzlj@users.noreply.github.com>
2024-12-04 14:05:53 +0800

    Update douyin_guaji.py

--- douyin_guaji.py
+++ douyin_guaji.py
@@ -6,11 +6,11 @@ class fudai_guaji:
     def __init__(self):
         self.analyser = fudai_analyse()
 
-    def guaji(self, y_pianyi=0, y_resolution=2400):
+    def guaji(self, y_pianyi=0, x_resolution=1080, y_resolution=2400):
         """根据自己截图的实际情况，修改y轴即高度的偏移值"""
         device_id = self.analyser.select_device()
         if device_id:
-            self.analyser.fudai_choujiang(device_id, y_pianyi, y_resolution, True, 5)
+            self.analyser.fudai_choujiang(device_id, y_pianyi, x_resolution, y_resolution, True, 5)
             return True
         time.sleep(10)
         self.guaji(y_pianyi)
@@ -18,4 +18,4 @@ class fudai_guaji:
 
 if __name__ == '__main__':
     choujiang = fudai_guaji()
-    choujiang.guaji(0, 2400)  # 参数第一个是y的偏移值，第二个是你手机y的分辨率
+    choujiang.guaji(0, 1080, 2400)  # 参数第1个是y的偏移值，第2个是你手机横轴的分辨率，第3个是你手机纵轴的分辨率
